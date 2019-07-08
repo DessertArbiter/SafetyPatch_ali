6f97393b196ad16b4174ae60b6d68f7753c668c5
tim <arbiterofdessert@gmail.com>
2019-07-07 20:58:48 -0400

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -7,3 +7,12 @@
 MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
+sed 's/ORANGE/GREEN/i' /proc/cmdline | sed 's/YELLOW/GREEN/i' > /data/local/tmp/cmdline
+mount -o bind /data/local/tmp/cmdline /proc/cmdline
+
+sed 's;^ro.*\.build\.fingerprint=.*;ro.build.fingerprint=motorola/ali/ali:9/PPS29.55-37-4/908b8:user/release-keys;' /system/build.prop > /data/local/tmp/build.prop
+mount -o bind /data/local/tmp/build.prop /system/build.prop
+# The build.prop thing seems to be useless, but at some point Google are sure to catch on and check in the raw build.prop to see if we're abusing getprop/resetprop.
+
+resetprop ro.build.fingerprint 'motorola/ali/ali:9/PPS29.55-37-4/908b8:user/release-keys'
+resetprop ro.bootimage.build.fingerprint 'motorola/ali/ali_retail:9/PPS29.55-37-4/908b8:user/release-keys'
